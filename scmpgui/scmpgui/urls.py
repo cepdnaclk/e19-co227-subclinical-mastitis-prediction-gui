9573df2c475adf4bf422ab8d4d19ec46e756d04a
Sahan Nimantha <115460116+SahanNimantha99@users.noreply.github.com>
2023-09-18 20:03:22 +0530

    update project data

--- scmpgui/scmpgui/urls.py
+++ scmpgui/scmpgui/urls.py
@@ -29,5 +29,7 @@ urlpatterns = [
     path('', include('csvR.urls'))
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
