048cdb1d64499b8dba9758e395046106bf799e06
Sahan Nimantha <115460116+SahanNimantha99@users.noreply.github.com>
2023-09-26 03:44:50 +0530

    Revert "Merge remote-tracking branch 'origin/authentication' into csv"
    This reverts commit 2d150ec98d03dab610a38300c0a76a53d6a2f68d, reversing
    changes made to 0ce96279a5c405a66614fafc69ccb6690840c9b5.

--- scmpgui/members/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login_page/', views.login_page, name='login'),
-    path('', views.register_page, name='register'),
-] 
