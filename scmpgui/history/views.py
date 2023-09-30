a429bbc9a229cbf7cea7e0ff4a59ff7c5556f794
CColombus <115539695+CColombus@users.noreply.github.com>
2023-10-01 04:00:23 +0530

    Added a url to load initial data

--- scmpgui/history/views.py
+++ scmpgui/history/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import HistoricalRecords
+from .files import InitHistory
 
 # Create your views here.
 def InitData(request):
-    #TODO Implement methods to load data into model
-    return HttpResponse("Load to database goes here!")
+    InitHistory()
+    return HttpResponse("Databse Loaded!")
 
 def ViewHistory(request):
     #TODO Historical Data Visulization View
