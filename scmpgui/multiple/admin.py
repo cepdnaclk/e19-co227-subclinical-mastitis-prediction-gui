93f91bf7f1aa22d76ce86960222600282a2f4ab7
Sahan Nimantha <115460116+SahanNimantha99@users.noreply.github.com>
2023-10-04 01:08:00 +0530

    solved some errors

--- scmpgui/multiple/admin.py
+++ scmpgui/multiple/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 from import_export.admin import ImportExportModelAdmin
 
-# admin.site.register(batchdataaset)
+# admin.site.register(item)
 @admin.register(Batchdataset)
 class ViewAdmin(ImportExportModelAdmin):
     exclude = ('id', )
