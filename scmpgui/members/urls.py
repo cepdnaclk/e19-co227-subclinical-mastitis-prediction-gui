c54f7c166d22afe9da64eb42e12a9e470e1d7fac
CColombus <115539695+CColombus@users.noreply.github.com>
2023-10-13 17:22:36 +0530

    Unsaved changes url redirected for root page

--- scmpgui/members/urls.py
+++ scmpgui/members/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     path('login/', views.login_page, name='members_login'),
     path('register/', views.register_page, name='members_register'),
     path('logout/', views.logout_user, name='members_logout'),
+    path('', RedirectView.as_view(url='login/',permanent=True))
 ] 
