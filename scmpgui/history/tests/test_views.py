36ae21a7da75e7dd73e2721b70df0710c2d50767
Dineth-Udugamasooriya <115542491+Dineth-Udugamasooriya@users.noreply.github.com>
2023-10-16 16:23:02 +0530

    testing initialized

---
+++ scmpgui/history/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from history.models import HistoricalRecords
+import json
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.list_url = reverse('history_main')
+
+    def test_project_list_GET(self):
+
+
+        response = self.client.get(reverse('history_main'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'history/main.html')
